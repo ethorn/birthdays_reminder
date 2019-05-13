ee3d2fafcd3d6253bb01cfede8513924793fde40
Eric Thörn <eric.m.thorn@gmail.com>
2019-05-13 19:13:32 +0200

    Cleaning up. Correcting Readme.

--- birthday_reminder_app.py
+++ birthday_reminder_app.py
@@ -8,14 +8,11 @@
 #    : sudo launchctl load -w ~/Library/LaunchAgents/com.birthdays.reminder.plist
 # To remove: sudo launchctl unload -w ~/Library/LaunchAgents/com.birthdays.reminder.plist
 
-# Neste steg:
-# * Få den til å kjøre i bakgrunnen og sjekke 2-3 ganger per dag
 
 import os
 import time
 
 
-# Take the birthday lookup file from home directory
 file_path = os.environ['HOME'] + '/Desktop/birthdays_reminder_app/birthdays_library.txt'
 
 
@@ -28,13 +25,11 @@ def notify(title, text):
 def check_birthday():
     lookup_file = open(file_path, 'r')
     today = time.strftime('%d-%m')
-    # birth_day_flag = 0
-    
+
     for entry in lookup_file:
         if today in entry:
             line = entry.split(' ')
-            # birth_day_flag = 1
-            notify('BURSDAG: ' + line[1] + ' ' + line[2], "Husk!")
+            notify(line[1] + ' ' + line[2], "Has birthday today!")
 
 
 check_birthday()
